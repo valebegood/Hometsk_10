ff34ff6b19e64f4085b4ab2c1a56148a1563fcf6
Valerii Chekanov <valebegood@gmail.com>
2024-03-15 01:01:23 +0100

    second com.

--- task#3.py
+++ task#3.py
@@ -5,7 +5,7 @@ import csv
 summary = []
 names = ('Mark', 'Mary', 'Josh', 'Kate', 'Luke')
 for name in names:
-    score = str(randrange(1000)) 
+    score = [randrange(1000) for _ in range (100)]
     summary.append({'Player name': name, 'Score': score})
     
 with open('summary.csv', 'w', newline='', encoding='utf-8') as f:
--- task#4.py
+++ task#4.py
@@ -3,11 +3,22 @@ import csv
 with open('summary.csv', 'r', encoding='utf8') as f:
     reader = csv.DictReader(f)
     reserv = list(reader)
-revers = sorted(reserv, key=lambda x: int(x['Score']), reverse=True)
-with open('new_summary.csv', 'w', encoding='utf8', newline ='') as new_f:
+    
+for row in reserv:
+    
+    scores = row['Score'].strip('[]').split(', ')
+    
+    scores = [int(score) for score in scores]
+    
+    row['Score'] = max(scores)
+
+
+revers = sorted(reserv, key=lambda x: x['Score'], reverse=True)
+
+
+with open('new_summary.csv', 'w', encoding='utf8', newline='') as new_f:
     fieldnames = reader.fieldnames
-    writer = csv.DictWriter(new_f, fieldnames=reader.fieldnames)
+    writer = csv.DictWriter(new_f, fieldnames=fieldnames)
     writer.writeheader()
     for row in revers:
-            new_f.write(f"{row['Player name']}, {row['Score']}\n")
-     
+        writer.writerow(row)     
